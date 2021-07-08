57df498822a59c88e6c7592842447bae224265c4
tomatoaas <tomomi.314159265@gmail.com>
2021-07-08 10:08:11 +0900

    user表のモックデータの作成

--- main.go
+++ main.go
@@ -7,13 +7,21 @@ import (
 
 type User struct {
 	USER_ID		string	`json:"id"`
-	NAME_ID		string	`json:"name"`
+	USER_NAME	string	`json:"name"`
 	PASSWORD	string	`json:"password"`
 }
+
+//Userのデータを保持するスライスの作成
+var users []User
 
 func main(){
 	//ルータのイニシャライズ
 	r := mix.NewRouter()
+
+	//モックデータの作成
+	users = append(users, User{USER_ID: "1", USER_NAME: "Yoshi", PASSWORD: "123qwe"})
+	users = append(users, User{USER_ID: "2", USER_NAME: "mura", PASSWORD: "123qwecc"})
+
 
 	//ルート（エンドポイント）
 	r.HandleFunc("api/user", getUser).Methods("GET")
