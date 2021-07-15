17b4b2ccf015d8c3fab08ee45afe6e8135a3b689
tomatoaas <tomomi.314159265@gmail.com>
2021-07-15 09:35:27 +0900

    envファイル追加

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/tomatoaas/GoAPI
+
+go 1.16
+
+require (
+	github.com/gollira/mux v1.6.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gollira/mux"
+	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
 )
 
-type User struct {
+type USER struct {
 	USER_ID		string	`json:"id"`
 	USER_NAME	string	`json:"name"`
 	PASSWORD	string	`json:"password"`
 }
 
 //Userのデータを保持するスライスの作成
-var users []User
+var users []USER
 
 //すべてのuserを取得する
 func getUsers(w http.ResponseWriter, r *http.Request){
@@ -37,14 +37,14 @@ func getUser(w http.ResponseWriter, r *http.Request){
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&User{})
+	json.NewEncoder(w).Encode(&USER{})
 }
 
 //新規ユーザーの追加
 func createUser(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 
-	var user User
+	var user USER
 	_= json.NewDecoder(r.Body).Decode(&user)
 	user.USER_ID = strconv.Itoa(rand.Intn(10000))
 	users = append(users, user)
@@ -60,7 +60,7 @@ func updateUser(w http.ResponseWriter, r *http.Request){
 	for index, item := range users{
 		if item.USER_ID == params["id"]{
 			users = append(users[:index], users[index+1:]...)
-			var user User
+			var user USER
 			_ = json.NewDecoder(r.Body).Decode(&user)
 			user.USER_ID = params["id"]
 			users = append(users, user)
@@ -89,11 +89,11 @@ func deleteUser(w http.ResponseWriter, r *http.Request){
 
 func main(){
 	//ルータのイニシャライズ
-	r := mix.NewRouter()
+	r := mux.NewRouter()
 
 	//モックデータの作成
-	users = append(users, User{USER_ID: "1", USER_NAME: "Yoshi", PASSWORD: "123qwe"})
-	users = append(users, User{USER_ID: "2", USER_NAME: "mura", PASSWORD: "123qwecc"})
+	users = append(users, USER{USER_ID: "1", USER_NAME: "Yoshi", PASSWORD: "123qwe"})
+	users = append(users, USER{USER_ID: "2", USER_NAME: "mura", PASSWORD: "123qwecc"})
 
 
 	//ルート（エンドポイント）
